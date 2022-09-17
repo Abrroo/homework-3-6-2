5405fad668585381617c19f7fadcdaa45b12a84f
Abrroo <akhmetov.ersyn@gmail.com>
2022-09-17 15:57:16 +0300

    gg

--- homework-3-6-2/homework-3-6-2.cpp
+++ homework-3-6-2/homework-3-6-2.cpp
@@ -1,8 +1,13 @@
-﻿
+﻿#define SUB(A, B) ((A) - (B))
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
 }
 
